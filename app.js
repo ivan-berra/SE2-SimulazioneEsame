1bcb7540ea7a8330b2c44808c39d1eab6bb25d6b
Ivan Berra <ivan.berra@studenti.unitn.it>
2018-12-04 19:06:17 +0100

    Finalizzato server base app

--- app.js
+++ app.js
@@ -2,24 +2,27 @@ const express = require('express');
 
 function string_square(s) {
 
-    if (typeof s === "string")
-        return { "result": s.length * s.length };
-    else return { "result": -1 };
+    if (s != null && s.charAt(0) == '"' && s.charAt(s.length - 1) == '"') {
+        var stringLength = s.length - 2;
+        return { "result": (stringLength * stringLength) };
+    } else return { "result": -1 };
 
 }
 
 module.exports = string_square;
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => console.log('Example app listening on port ' + PORT));
 
 app.get('/square', function(req, res) {
 
-    res.contentType(application / json);
+    res.setHeader('ContentType', 'application/json');
 
     var stringa = req.query.string;
+
+    console.log("Received: " + stringa);
 
     var result = string_square(stringa);
 
